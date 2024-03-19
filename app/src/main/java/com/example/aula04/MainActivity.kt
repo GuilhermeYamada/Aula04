9de9e89865b427e5e4d380b402c1dbe07e4ef15d
JGomesV <jvgsantos05@gmail.com>
2024-03-19 08:54:49 -0300

    Aula04-Compose

---
+++ app/src/main/java/com/example/aula04/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.aula04
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.aula04.ui.theme.Aula04Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Aula04Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Inicial()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TextCustomizado(texto : String) {
+    Text(
+        text = texto,
+        color = Color.White
+    )
+}
+
+@Composable
+fun Inicial() {
+    Column(
+        modifier = Modifier.background(color = Color.Blue)
+    ){
+        TextCustomizado(texto = "Terceira aula")
+            Row {
+                TextCustomizado(texto = "Android Studio")
+                TextCustomizado(texto = "JetPack Compose")
+            }
+    }
+}
